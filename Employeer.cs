6eec374abe20ca64ec7d7a8f96ed408cfbf99b38
Vũ Phạm Tiến Dũng <43530592+vuphamtiendung@users.noreply.github.com>
2022-12-26 17:42:21 +0700

    Add project files.

---
+++ Employeer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BNTN_Ass02_Opt1
+{
+    internal abstract class Employeer
+    {
+        private string _name;
+        private double _allowance;
+        private double _coefficientSalary;
+        private string _department;
+        private bool _role;
+        
+        public string Name { get { return _name; } set { _name = value; } }
+        public double Allowance { get { return _allowance;} set { if (_allowance >= 0) { _allowance = value; } } }
+        public double CoefficientSalary { get { return _coefficientSalary; } set { _coefficientSalary = value; } }
+        public string Department { get { return _department; } set { _department = value; } }
+        public bool Role { get { return _role; } set { if (_role == true || _role == false) { _role = value; } } }
+
+        public Employeer()
+        {
+            _name = "";
+            _allowance = 0;
+            _coefficientSalary = 0;
+        }
+        public Employeer(string name, double allowance, double coefficientSalary)
+        {
+            _name = name;
+            _allowance= allowance;
+            _coefficientSalary = coefficientSalary;
+        }
+
+        public abstract double Income(); // Income tax, use abstract
+
+        public virtual string InputInfo(string message)
+        {
+            WriteLine(message);
+            return ReadLine();
+        }
+
+        public virtual void InputEmployee()
+        {
+            _name = InputInfo(DisplayConstant.INPUT_NAME);
+            _allowance = Convert.ToDouble(InputInfo(DisplayConstant.INPUT_ALLOWANCE));
+            _coefficientSalary = Convert.ToDouble(InputInfo(DisplayConstant.INPUT_COEFFICIENTSALARY));
+            _department = InputInfo(DisplayConstant.INPUT_DEPARTMENT);
+            ReadLine();
+        }
+
+        public virtual void DisplayEmployee()
+        {
+            WriteLine(DisplayConstant.OUTPUT_NAME, _name);
+            WriteLine(DisplayConstant.OUTPUT_ALLOWANCE, _allowance);
+            WriteLine(DisplayConstant.OUTPUT_COEFFICIENT_SALARY, _coefficientSalary);
+            WriteLine(DisplayConstant.OUTPUT_DEPARTMENT, _department);
+            WriteLine(DisplayConstant.OUTPUT_INCOME, Income());
+        }
+    }
+}
---
+++ UserAction.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace BNTN_Ass02_Opt1
+{
+    internal class UserAction
+    {
+        public Teacher teacher = null;
+        public Employee employee = null;
+
+        public List<Teacher> listTeacher = new List<Teacher>();
+        public List<Employee> listEmployee = new List<Employee>();
+
+        public static UserAction useraction;
+        public UserAction() { }
+        public static UserAction GetUserAction()
+        {
+            if(useraction == null)
+            {
+                useraction = new UserAction();
+            }
+            return useraction;
+        }
+
+        public string InputInfo(string message)
+        {
+            WriteLine(message);
+            return ReadLine();
+        }
+
+        public enum Options
+        {
+            InputInfoEmployee = 1,
+            FindEmployee = 2,
+            SoftAndDisplay = 3,
+            Exit = 4
+        };
+
+
+        public List<Employee> SoftEmployee(List<Employee> listEmploy)
+        {
+            listEmploy = listEmployee.OrderByDescending(x => x.Income())
+                                    .ThenBy(y => y.Name)
+                                    .ToList();
+
+            foreach (var list in listEmploy)
+            {
+                WriteLine(DisplayConstant.OUTPUT_NAME, list.Name);
+                WriteLine(DisplayConstant.OUTPUT_ALLOWANCE, list.Allowance);
+                WriteLine(DisplayConstant.OUTPUT_COEFFICIENT_SALARY, list.CoefficientSalary);
+                WriteLine(DisplayConstant.OUTPUT_DEPARTMENT, list.Department);
+                WriteLine(DisplayConstant.OUTPUT_INCOME, list.Income());
+                WriteLine();
+            }
+            return listEmploy;
+        }
+
+        public List<Teacher> SoftTeacher(List<Teacher> listTeach)
+        {
+            listTeach = listTeacher.OrderBy(x => x.Income())
+                                    .ThenBy(y => y.Name)
+                                    .ToList();
+
+            foreach (var list in listTeach)
+            {
+                WriteLine(DisplayConstant.OUTPUT_NAME, list.Name);
+                WriteLine(DisplayConstant.OUTPUT_ALLOWANCE, list.Allowance);
+                WriteLine(DisplayConstant.OUTPUT_COEFFICIENT_SALARY, list.CoefficientSalary);
+                WriteLine(DisplayConstant.OUTPUT_DEPARTMENT, list.Department);
+                WriteLine(DisplayConstant.OUTPUT_INCOME, list.Income());
+                WriteLine();
+            }
+            return listTeach;
+        }
+
+        public void Perform()
+        {
+            Options option;
+            do
+            {
+                WriteLine(DisplayConstant.INPUT_EMPLOYEE_INFO);
+                WriteLine(DisplayConstant.MENU_START);
+                WriteLine(DisplayConstant.MENU_CREATE_EMPLOYEE);
+                WriteLine();
+                WriteLine(DisplayConstant.MENU_INFOR_INCOME_EMPLOYEE);
+                WriteLine();
+                WriteLine(DisplayConstant.MENU_DETAIL_ALLOWANCE_TEACHER);
+                WriteLine();
+                WriteLine(DisplayConstant.MENU_DETAIL_ALLOWANCE_MANAGER);
+                WriteLine();
+                WriteLine(DisplayConstant.MENU_FIND);
+                WriteLine();
+                WriteLine(DisplayConstant.MENU_DISPLAY_EMPLOYEE);
+                WriteLine();
+                WriteLine(DisplayConstant.MENU_EXIT);
+                WriteLine(DisplayConstant.END_OF_PAGE_MESSAGE);
+
+                Enum.TryParse(ReadLine(), out option);
+                switch (option)
+                {
+                    case Options.InputInfoEmployee:
+                        {
+                            try
+                            {
+                                bool choose = Convert.ToBoolean(InputInfo(DisplayConstant.INPUT_ROLE));
+                                if(choose == true)
+                                {
+                                    employee = new Employee();
+                                    employee.InputEmployee();
+                                    listEmployee.Add(employee);
+                                }
+                                else if(choose == false)
+                                {
+                                    teacher = new Teacher();
+                                    teacher.InputEmployee();
+                                    listTeacher.Add(teacher);
+                                }
+                            }
+                            catch(Exception ex)
+                            {
+                                WriteLine(DisplayConstant.OUTPUT_ERROR_DEFINE);
+                                WriteLine(ex.ToString());
+                            }                        
+                            break;
+                        }
+                    case Options.FindEmployee:
+                        {
+                            try
+                            {
+                                string name = InputInfo(DisplayConstant.INPUT_FINDNAME);
+                                string department = InputInfo(DisplayConstant.INPUT_FINDDEPARTMENT);
+
+                                var listEmployeeFind = listTeacher.FindAll(x => x.Name == name && x.Department == department);
+
+                                if (listEmployeeFind != null)
+                                {
+                                    foreach (var list in listEmployeeFind)
+                                    {
+                                        WriteLine();
+                                        WriteLine(DisplayConstant.OUTPUT_NAME, list.Name);
+                                        WriteLine(DisplayConstant.OUTPUT_ALLOWANCE, list.Allowance);
+                                        WriteLine(DisplayConstant.OUTPUT_COEFFICIENT_SALARY, list.CoefficientSalary);
+                                        WriteLine(DisplayConstant.OUTPUT_INCOME, list.Income());
+                                        WriteLine(DisplayConstant.OUTPUT_DEPARTMENT, list.Department);
+                                        WriteLine(DisplayConstant.OUTPUT_LEVEL, list.Level);
+                                        WriteLine(DisplayConstant.OUTPUT_NUMBER_LECTURE, list.NumberLecture);
+                                        WriteLine();
+                                    }
+                                }
+                                else
+                                {
+                                    WriteLine(DisplayConstant.OUTPUT_CANNOT_FIND);
+                                }
+
+                                var listTeacherFind = listEmployee.FindAll(x => x.Name == name && x.Department == department);
+
+                                if(listTeacherFind != null)
+                                { 
+
+                                    foreach (var list in listTeacherFind)
+                                    {
+                                        WriteLine();
+                                        WriteLine(DisplayConstant.OUTPUT_NAME, list.Name);
+                                        WriteLine(DisplayConstant.OUTPUT_ALLOWANCE, list.Allowance);
+                                        WriteLine(DisplayConstant.OUTPUT_COEFFICIENT_SALARY, list.CoefficientSalary);
+                                        WriteLine(DisplayConstant.OUTPUT_INCOME, list.Income());
+                                        WriteLine(DisplayConstant.OUTPUT_DEPARTMENT, list.Department);
+                                        WriteLine(DisplayConstant.OUTPUT_NUMBERWORKDAY, list.NumberWorkDay);
+                                        WriteLine(DisplayConstant.OUTPUT_ROLE_MANAGER, list.Position);
+                                        WriteLine();
+                                    }
+                                }
+                                else
+                                {
+                                    WriteLine(DisplayConstant.OUTPUT_CANNOT_FIND);
+                                }
+                            }
+                            catch(Exception ex)
+                            {
+                                WriteLine(ex.ToString());
+                            }
+                            break;
+                        }
+                    case Options.SoftAndDisplay:
+                        {
+                            WriteLine(DisplayConstant.OUTPUT_EMPLOYEE);
+                            SoftEmployee(listEmployee);
+                            WriteLine();
+                            WriteLine(DisplayConstant.OUTPUT_TEACHER);
+                            SoftTeacher(listTeacher);
+                            break;
+                        }
+                    case Options.Exit:
+                        {
+                            WriteLine(DisplayConstant.MENU_EXIT);
+                            break;
+                        }
+                    default:
+                        {
+                            WriteLine(DisplayConstant.MENU_DONT_FUNCTION);
+                            break;
+                        }
+                }
+            }
+            while (option != Options.Exit);
+        }
+    }
+}
